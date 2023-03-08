83557fd55f43fec86a1bf3521009d11baf89a1f3
Hien22012008 <xuanhiennguyen34@gmail.com>
2023-03-08 13:53:34 +0700

    Woman's day

---
+++ index.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+    document.querySelector("#chocolate_rains").hidden = false;
+    document.querySelector("#hide_2").hidden = false;
+    document.querySelector("#hide_3").hidden = false;
+
+    document.querySelector("body").style.backgroundColor='#fff';
+}, 6000);
